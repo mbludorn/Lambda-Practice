b0314e8b5a4d8ebff21290042d51e9a8c5495dad
SeedPaths-User <lessdismissive@email.com>
2014-09-09 10:49:47 -0600

    Morgan Lambda Practice

--- Lambda Practice/Lanmbda Practice/Lanmbda Practice/Program.cs
+++ Lambda Practice/Lanmbda Practice/Lanmbda Practice/Program.cs
@@ -3,13 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Lanmbda_Practice
+namespace LambdaPractice
 {
     class Program
     {
+        public void Greet();
+    //call the Function Greet();
+   
+
         static void Main(string[] args)
         {
-        }
-    }
-}
+            List<string> products = new List<string>() {"Basketball", "Baseball", "Tennis Raquet", "Running Shoes", "Wrestling Shoes", 77777777
+                "Soccer Ball", "Football", "Shoulder Pads", 
+                "Trail Running Shoes", "Cycling Shoes", "Kayak", "Kayak Paddles"};
+
+
+            //declare a variable kayakProduces and set it equal to all products that contain the word "Kayak"
+            List<string> kayakProduces = products.Where(x => x.Contains("kayak")).ToList();                                           
+           
+                  
+            //print the kayakProducts to the console using a foreach loop.
+            Console.WriteLine(string.Join(" ,", kayakProduces));
+           
+            
+            //declare a variable shoeProducts and set it equal to all products that contain the word "Shoes"
+            List<string> shoeProducts =products.Where(x => x.Contains("Shoes")).ToList();
+           
+            
+            //print the shoeProducts to the console using a foreach loop or string.Join().
+            Console.WriteLine(string.Join(" ,", shoeProducts));
+           
+            
+            //declare a variable ballProducts and set it equal to all the products that have ball in the name.
+            List<string> ballProducts = products.Where(x => x.Contains("balls")).ToList();
+            
+            
+            //print the ballProducts to the console using a foreach loop or string.Join().
+            Console.WriteLine(string.Join(" ,", ballProducts)); 
+            
+           
+
+            //sort ballProducts alphabetically and print them to the console.
+            List<string> newballProducts = products.OrderBy(x => x.Length).ThenBy(x => x).ToList();
+           
+            
+            //add six more items to the products list using .add();
+            List<string> newProducts 
+            Console.WriteLine(";
+            Console.WriteLine = 
+
+            //print the product with the longest name to the console using the .First() extension.
+            //order the list by length, and print them out to the console.
+            //order by sorts by length, then sort by alphabetically 
+            List<string> newProduct = newProducts++0rderBydescending(x => x.Length).ThenBy(x => x).ToList();
+            {          
+                Console.WriteLine(20);
+            }
+       
+            
+            //print the product with the shortest name to the console using OrderByDesceding() and the .First() extension.
+             List<string> sorted = newProduct.OrderByDesending(x => x.Length).ThenBy(x => x).ToList().First();
+            
+           
+            //Print the 3 product with the 4th shortest name to the console using an index or Skip/Take (you must convert the results to a list using .ToList()).
+            List<string> sortedim sk tured = Patricia.OrderByDesending(x => x.LengthThenBy(x => x).ToList();
+            List<string> (x => x.Length).ToList()\ Console.WriteLine(sorted.Skip(3).Take(1).First());
+
+
+            //print the ballProduct with the 2nd longest name to the console using an index or Skip/Take (you must convert the results to a list using .ToList()).
+
+            //declare a variable reversedProducts and set it equal to all products ordered by the longest word first. (use the OrderByDescending() extension).
+
+            //print out the reversedProducts to the console using a foreach loop.
+
+            //print out all the products ordered by the longest word first using the OrderByDecending() extension and a foreach loop.  
+            //Note: you will not use a variable to store your list
+
+            Console.ReadKey();
+         }
+      
+    
